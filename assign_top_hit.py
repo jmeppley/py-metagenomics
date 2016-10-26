20f3c5b715788848d0d59f10c28d001c2b024d2e
Marmaduke <jmeppley@gmail.com>
2016-10-26 12:54:20 -1000

    more pep8 for scripts

--- assign_top_hit.py
+++ assign_top_hit.py
@@ -1,31 +1,68 @@
 #! /usr/bin/env python
 """
-Takes an m8 blast and picks the best hit for each. 
+Takes an m8 blast and picks the best hit for each.
 
-There are two count methods available, and these are not listed in the options below: 'tophit' and 'toporg'. The other count methods (first, most, etc) are unavailable.
+There are two count methods available, and these are not listed in the
+options below: 'tophit' and 'toporg'. The other count methods (first,
+most, etc) are unavailable.
 
-First, only the best scores are used, but if there is a tie (aka ambiguous hit), than a winner is assigned using the abundances found from unambiguous hits. When 'tophit' is selected, the hit with the highest overal abundance (from unambigous reads) is used. When 'toporg' is used, the hit which belongs to the most abundant taxon is used.
+First, only the best scores are used, but if there is a tie (aka ambiguous
+hit), than a winner is assigned using the abundances found from unambiguous
+hits. When 'tophit' is selected, the hit with the highest overal abundance
+(from unambigous reads) is used. When 'toporg' is used, the hit which belongs
+to the most abundant taxon is used.
 
-If the -P or --proportinal flag is given, then ambiguous hits are resolved so that the overall proportion of hit (when using tophit) or taxon (for toporg) abundance is changed the least.
+If the -P or --proportinal flag is given, then ambiguous hits are resolved
+so that the overall proportion of hit (when using tophit) or taxon (for
+toporg) abundance is changed the least.
 
 FilterPct defaults to 0, but can be altered, but I don't recommend it.
 """
 
-import sys, re, logging, argparse
+import sys
+import re
+import logging
+import argparse
 from edl import redistribute
 from edl.hits import *
 from edl.util import *
 from edl.blastm8 import M8Stream
 from urllib.parse import unquote_plus
 
+
 def main():
     usage = "usage: %prog [OPTIONS] HIT_TABLE(S)"
     description = __doc__
     parser = argparse.ArgumentParser(description=description)
     add_IO_arguments(parser)
-    add_taxon_arguments(parser,defaults={'filterPct':0,'parseStyle':ACCS,'countMethod':'tophit'},choices={'countMethod':('tophit','toporg')})
-    parser.add_argument("-P","--proportional", dest="proportional", default=False, action="store_true", help="Assign reads that have multiple equal top hits to taxa such that the overal proportion of taxa is consistent with the unambiguious hits. This is meant for use with the 'toporg' count method.")
-    parser.add_argument("-i","--individualFiles", dest="individual", default=False, action="store_true", help="Use this flag to process files independently. Normally, counts from all files are pooled for making choices.")
+    add_taxon_arguments(
+        parser,
+        defaults={
+            'filterPct': 0,
+            'parseStyle': ACCS,
+            'countMethod': 'tophit'},
+        choices={
+            'countMethod': (
+                'tophit',
+                'toporg')})
+    parser.add_argument(
+        "-P",
+        "--proportional",
+        dest="proportional",
+        default=False,
+        action="store_true",
+        help="Assign reads that have multiple equal top hits to taxa such "
+             "that the overal proportion of taxa is consistent with the "
+             "unambiguious hits. This is meant for use with the 'toporg' "
+             "count method.")
+    parser.add_argument(
+        "-i",
+        "--individualFiles",
+        dest="individual",
+        default=False,
+        action="store_true",
+        help="Use this flag to process files independently. Normally, "
+             "counts from all files are pooled for making choices.")
 
     add_universal_arguments(parser)
     arguments = parser.parse_args()
@@ -33,33 +70,38 @@ def main():
 
     # load necessary maps
     params = FilterParams.create_from_arguments(arguments)
-    if arguments.countMethod=='toporg':
-        (taxonomy,hitStringMap)=readMaps(arguments)
+    if arguments.countMethod == 'toporg':
+        (taxonomy, hitStringMap) = readMaps(arguments)
 
     wta = not(arguments.proportional)
 
-    if len(arguments.input_files)<=1 or arguments.individual:
+    if len(arguments.input_files) <= 1 or arguments.individual:
         # loop over input
-        for (inhandle,outhandle) in inputIterator(arguments):
-            logging.debug("Reading from %s and writing to %s" % (inhandle, outhandle))
+        for (inhandle, outhandle) in inputIterator(arguments):
+            logging.debug(
+                "Reading from %s and writing to %s" %
+                (inhandle, outhandle))
 
-            m8stream=M8Stream(inhandle)
+            m8stream = M8Stream(inhandle)
             if arguments.countMethod == 'tophit':
-                # don't give any taxonomy, just map to accessions for redistribution
-                readHits = redistribute.pickBestHitByAbundance(m8stream,
-                                                      filterParams=params,
-                                                      returnLines=True,
-                                                      winnerTakeAll=wta,
-                                                      parseStyle=arguments.parseStyle)
+                # don't give any taxonomy, just map to accessions for
+                # redistribution
+                readHits = redistribute.pickBestHitByAbundance(
+                    m8stream,
+                    filterParams=params,
+                    returnLines=True,
+                    winnerTakeAll=wta,
+                    parseStyle=arguments.parseStyle)
             else:
                 # translate to organism before finding most abundant
-                readHits = redistribute.pickBestHitByAbundance(m8stream,
-                                                          filterParams=params,
-                                                          returnLines=True,
-                                                          winnerTakeAll=wta,
-                                                          taxonomy=taxonomy,
-                                                          hitStringMap=hitStringMap,
-                                                          parseStyle=arguments.parseStyle)
+                readHits = redistribute.pickBestHitByAbundance(
+                    m8stream,
+                    filterParams=params,
+                    returnLines=True,
+                    winnerTakeAll=wta,
+                    taxonomy=taxonomy,
+                    hitStringMap=hitStringMap,
+                    parseStyle=arguments.parseStyle)
 
             for line in readHits:
                 outhandle.write(line)
@@ -69,46 +111,47 @@ def main():
         multifile = redistribute.multipleFileWrapper(arguments.input_files)
 
         # Build a map from input file name to output handle
-        outputMap={}
+        outputMap = {}
         for infile_handle in arguments.input_files:
             infile_name = infile_handle.name
             if arguments.output_file is None:
-                outputMap[infile_name]=sys.stdout
-            elif len(arguments.input_files)<=1:
-                outputMap[infile_name]=open(arguments.output_file,'w')
+                outputMap[infile_name] = sys.stdout
+            elif len(arguments.input_files) <= 1:
+                outputMap[infile_name] = open(arguments.output_file, 'w')
             else:
                 # use outfileName as suffix
                 if arguments.cwd:
                     # strip path info first
-                    (infilePath,infileFile)=os.path.split(infile_name)
-                    outfile="./"+infileFile+arguments.output_file
+                    (infilePath, infileFile) = os.path.split(infile_name)
+                    outfile = "./" + infileFile + arguments.output_file
                 else:
-                    outfile=infile_name+arguments.output_file
-                outputMap[infile_name]=open(outfile,'w')
-
+                    outfile = infile_name + arguments.output_file
+                outputMap[infile_name] = open(outfile, 'w')
 
         if arguments.countMethod == 'tophit':
-            # don't give any taxonomy, just map to accessions for redistribution
-            readHits = redistribute.pickBestHitByAbundance(multifile,
-                                                  filterParams=params,
-                                                  returnLines=False,
-                                                  winnerTakeAll=wta,
-                                                  parseStyle=arguments.parseStyle)
+            # don't give any taxonomy, just map to accessions for
+            # redistribution
+            readHits = redistribute.pickBestHitByAbundance(
+                multifile,
+                filterParams=params,
+                returnLines=False,
+                winnerTakeAll=wta,
+                parseStyle=arguments.parseStyle)
         else:
             # translate to organism before finding most abundant
-            readHits = redistribute.pickBestHitByAbundance(multifile,
-                                                      filterParams=params,
-                                                      returnLines=False,
-                                                      winnerTakeAll=wta,
-                                                      taxonomy=taxonomy,
-                                                      hitStringMap=hitStringMap,
-                                                      parseStyle=arguments.parseStyle)
-
+            readHits = redistribute.pickBestHitByAbundance(
+                multifile,
+                filterParams=params,
+                returnLines=False,
+                winnerTakeAll=wta,
+                taxonomy=taxonomy,
+                hitStringMap=hitStringMap,
+                parseStyle=arguments.parseStyle)
 
         for (read, hit) in readHits:
-            infile_name, read = read.split("/",1)
+            infile_name, read = read.split("/", 1)
             outhandle = outputMap[unquote_plus(infile_name)]
-            outhandle.write(hit.line.split("/",1)[1])
+            outhandle.write(hit.line.split("/", 1)[1])
 
         if arguments.output_file is not None:
             for outhandle in outputMap.values():
@@ -116,4 +159,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
