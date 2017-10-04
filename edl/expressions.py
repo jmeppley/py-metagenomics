f39412f5771ca749e927453c4b4993e4ca39d0f0
Marmaduke <jmeppley@gmail.com>
2017-10-04 07:36:12 -1000

    making the accession regular expression more generic

--- edl/expressions.py
+++ edl/expressions.py
@@ -3,15 +3,15 @@ from edl.kegg import koRE
 
 fastaRE = re.compile(r'^>(\S+)(?:\s+(\S.*))?$')
 accessionRE = re.compile(
-    r"""\b                # start at a word-break
-        (?:edl|dbj|emb|gb|pdb|pir|prf|ref|sp|tpd|tpe|tpg|CDD|lcl)
-                            # one of the seq codes
-        \|{1,2}             # followed by one or two vertical bars
+    r"""(?:^|\b             # start at first chr or a word-break
+        [A-Za-z]{2,3}       # followed by one of the seq codes
+        \|{1,2})            # and one or two vertical bars
         ([-\.0-9a-zA-Z_]+)  # the accession is word chars plus . and - and _
         (?<!\.\d\d)(?<!\.\d\d\d)(?<!\.\d)
                             # dont include version suffix (eg .1 or .11)
         (?:\.\d+)?          # allow for version suffix outside of match
-        (?:\||$)            # must end with bar (|) or nothing ($)""",
+        (?:\||$)            # must end with bar (|) or nothing ($)
+        (?=(\s|$))          # either at end or before whitespace""",
     re.X)
 nrOrgRE = re.compile(
     r'\[([^|\[\]]*(?:\[[^|\[\]]+\])?[^|\[\]]*)\]?\s*(?:(?=[a-z]{2,3}\|)|$)')
